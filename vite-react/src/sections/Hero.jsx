29749188dc6b5d394995b3f44e58ba80588dbfd9
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-09 23:44:34 +0000

    Canvas and Camera added. Frameworks installed

--- vite-react/src/sections/Hero.jsx
+++ vite-react/src/sections/Hero.jsx
@@ -1,6 +1,19 @@
+import { Canvas } from "@react-three/fiber"
+import { PerspectiveCamera } from "@react-three/drei"
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="min-h-screen w-full flex flex-col relative">
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Somuna.<span className="waving-hand">👋🏽</span></p>
+            <p className="hero_tag text-gray_gradient">Building Software</p>
+        </div>
+        
+        <div className="w-full h-full absolute inset-0">
+            <Canvas className="w-full h-full">
+                <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+            </Canvas>
+        </div>
+    </section>
   )
 }
 
