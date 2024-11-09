b9b15f443a3903e7754c5daf367551c45c18b515
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-09 23:28:53 +0000

    responsive navbar added

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Navbar from './section/Navbar'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
-      <h1 className="text-2xl text-white underline">Hello, Three.js!</h1>
       <Navbar />
+      <Hero />
     </main>
   )
 }
