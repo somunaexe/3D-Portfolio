f70e448b6962272642a8987dcc8e91ec896d9cf2
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-15 16:19:39 +0000

    Links have now been refactored into one file to improve code reuse and are displayed in the navbar and footer

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { navLinks } from "../constants/index.js"
-
+import Links from "../components/Links.jsx"
 const NavItems = () => {
   return (
     <ul className="nav-ul">
@@ -22,10 +22,14 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
         <div className="max-w-7xl mx-auto">
             <div className="flex justify-between items-center py-5 mx-auto c-space">
-                <a href="/" 
-                className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
-                    SuperSomu
-                </a>
+                <div className="flex items-center gap-5">
+                    <a 
+                        href="/" 
+                        className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                        SuperSomu
+                    </a>
+                    <Links />
+                </div>
                 <button
                 onClick={toggleMenu}
                 className="
